385e5645b0b033d8ecb9bfeefe4b031bad9abc13
sandeepnewa10 <sandeepnewa@gmail.com>
2024-04-28 21:13:15 +1000

    create 404 page

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,7 @@
 // Put your application javascript here
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+});
