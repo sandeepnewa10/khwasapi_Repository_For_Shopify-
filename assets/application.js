921882e45533636755241c7d37e67e897f7dfa95
sandeepnewa10 <sandeepnewa@gmail.com>
2024-05-15 11:20:55 +1000

    delete all the project file

--- assets/application.js
+++
@@ -1,7 +0,0 @@
-// Put your application javascript here
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-});
